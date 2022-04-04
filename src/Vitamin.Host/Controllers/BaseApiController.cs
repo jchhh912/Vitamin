5a6f95c1856045858c71665d80521a845f3255a7
jchhh912 <jchhh912@163.com>
2022-04-05 00:27:24 +0800

    feat:创建Tokens Api

---
+++ src/Vitamin.Host/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vitamin.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        private ISender _mediator = null!;
+        /// <summary>
+        /// 绑定当前请求的IServiceProvider对象
+        /// </summary>
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
---
+++ src/Vitamin.Host/Controllers/Identity/TokensController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Identity.Requests;
+using Application.Identity.Responses;
+using Application.Identity.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vitamin.Host.Controllers.Identity
+{
+    public class TokensController:BaseApiController
+    {
+        private readonly ITokenService _tokenService;
+        public TokensController(ITokenService tokenService) => _tokenService = tokenService;
+
+        [HttpPost]
+        [AllowAnonymous]
+        public Task<TokenResponse> GetTokenAsync(TokenRequest request, CancellationToken cancellationToken)
+        {
+            return _tokenService.GetTokenAsync(request, GetIpAddress(), cancellationToken);
+        }
+        [HttpPost("refresh")]
+        [AllowAnonymous]
+        public Task<TokenResponse> RefreshAsync(RefreshTokenRequest request)
+        {
+            return _tokenService.RefreshTokenAsync(request, GetIpAddress());
+        }
+        private string GetIpAddress() =>
+    Request.Headers.ContainsKey("X-Forwarded-For")
+        ? Request.Headers["X-Forwarded-For"]
+        : HttpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString() ?? "N/A";
+    }
+}
