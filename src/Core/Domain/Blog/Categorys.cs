b4b0072f745640a3038814a789822f64cd7dada8
jchhh912 <jchhh912@163.com>
2022-04-10 20:46:02 +0800

    chore:初始化传入参数

--- src/Core/Domain/Blog/Categorys.cs
+++ src/Core/Domain/Blog/Categorys.cs
@@ -4,6 +4,17 @@ namespace Domain.Blog;
 
 public class Categorys
 {
+    public Categorys(string displayName,string note) 
+    {
+        if (string.IsNullOrEmpty(displayName))
+        {
+            DisplayName = displayName;
+        }
+        if (string.IsNullOrEmpty(note))
+        {
+            Note = note;
+        }
+    }
     public Guid Id { get; set; }
     /// <summary>
     /// 显示名称
