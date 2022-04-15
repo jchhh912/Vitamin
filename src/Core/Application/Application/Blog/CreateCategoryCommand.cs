fb350e24ec6ba0fa6e9dcc343250347b91d482b7
leichi <1871895196@qq.com>
2022-04-15 23:37:21 +0800

    fix:分类增删改查

---
+++ src/Core/Application/Application/Blog/CreateCategoryCommand.cs
@@ -0,0 +1,31 @@
+﻿using Application.Blog.Request;
+using Application.Presistence;
+using Domain.Blog;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Blog
+{
+    public record CreateCategoryCommand(CreateCategoryRequest Payload) : IRequest<Guid>;
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, Guid>
+    {
+        private readonly IRepository<Categorys> _categorysRepository;
+        public CreateCategoryCommandHandler(
+            IRepository<Categorys> categorysRepository)
+        {
+            _categorysRepository = categorysRepository;
+        }
+
+        public async Task<Guid> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var cate = new Categorys(request.Payload.DisplayName, request.Payload.Note);
+            cate.PostId = request.Payload.PostId;
+            await _categorysRepository.AddAsync(cate);
+            return cate.Id;
+        }
+    }
+}
---
+++ src/Core/Application/Application/Blog/Request/CreateCategoryRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Blog.Request
+{
+    public record CreateCategoryRequest
+    {
+        public Guid PostId { get; set; }
+        /// <summary>
+        /// 显示名称
+        /// </summary>
+        public string DisplayName { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Note { get; set; }
+    }
+}
